0b918a1d6d6401344455d90228cbb9ba778cdf08
Emmanuel Villegas Gonzalez <emmanuelvillegas01@gmail.com>
2019-01-27 20:01:41 -0400

    Add MaterialSelectField for reduxForm

--- src/utils/FieldsMaterialUI.jsx
+++ src/utils/FieldsMaterialUI.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // Material UI
-import { TextField } from '@material-ui/core';
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  FormHelperText,
+  TextField,
+  MenuItem
+} from '@material-ui/core';
+import { InfoOutlined } from '@material-ui/icons';
 
 export const FieldMaterialUI = props => {
   const {
@@ -15,5 +23,55 @@ export const FieldMaterialUI = props => {
       error={touched && invalid}
       helperText={touched && error}
     />
+  );
+};
+
+// component={FieldMaterialUI}
+//             id="inputName"
+//             name="Nombre"
+//             label="Nombre"
+//             margin="dense"
+//             variant="outlined"
+//             fullWidth
+//             type="text"
+
+const renderFromHelper = ({ touched, error }) => {
+  if (!(touched && error)) {
+    return;
+  } else {
+    return <FormHelperText>{touched && error}</FormHelperText>;
+  }
+};
+
+export const FieldMaterialUISelect = props => {
+  console.log(props);
+  const {
+    input,
+    label,
+    name,
+    meta: { touched, error },
+    options,
+    ...custom
+  } = props;
+  return (
+    <FormControl error={touched && error} fullWidth>
+      <InputLabel htmlFor={`input${input.name}`}>{label}</InputLabel>
+      <Select
+        {...input}
+        {...custom}
+        inputProps={{
+          name: input.name,
+          id: `input${input.name}`
+        }}
+      >
+        {options &&
+          options.map((item, index) => (
+            <MenuItem key={`${item.name}_${index}`} value={item.value}>
+              {item.name}
+            </MenuItem>
+          ))}
+      </Select>
+      {renderFromHelper({ touched, error })}
+    </FormControl>
   );
 };
