25beae19f4973d9ddb062d74b866735c14c12381
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-03-03 12:40:43 -0400

    new colors for theme Blue

--- src/themes/Blue.js
+++ src/themes/Blue.js
@@ -1,20 +1,20 @@
 // Material UI Theme
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createMuiTheme } from '@material-ui/core/styles';
 
 // Ant Design Pro Colors
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#1b90f7",
-      light: "#096dd9",
-      dark: "#40a9ff",
-      contrastText: "#ffffff"
+      main: '#2196f3',
+      light: '#64b5f6',
+      dark: '#1976d2',
+      contrastText: '#fff'
     },
     secondary: {
-      main: "#ffffff",
-      light: "#ffffff",
-      dark: "#cccccc",
-      contrastText: "rgba(0,0,0,0.65)"
+      main: '#fff',
+      light: '#fff',
+      dark: '#ccc',
+      contrastText: 'rgba(0,0,0,0.65)'
     }
   },
   // shape: {
@@ -22,7 +22,7 @@ const theme = createMuiTheme({
   // },
   typography: {
     useNextVariants: true,
-    fontFamily: "Roboto, Google Sans"
+    fontFamily: 'Roboto, Google Sans'
   },
   drawerWidth: 260
 });
