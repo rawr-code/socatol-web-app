9a18125e12300f552f62856964a284840ccdd750
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-04-13 15:25:43 -0400

    clean

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import jwt from 'jsonwebtoken';
+import { BrowserRouter } from 'react-router-dom';
+// import jwt from 'jsonwebtoken';
 
 // Utils
-import setAuthorizationToken from './utils/setAuthorizationToken';
+// import setAuthorizationToken from './utils/setAuthorizationToken';
 
-// Redux Store
-import store from './store';
-
-import { setCurrentUser, Logout } from './store/actions/Auth';
 import App from './App';
 
-if (localStorage.token) {
-  setAuthorizationToken(localStorage.token);
-  const decoded = jwt.decode(localStorage.token);
-  const currentTime = Date.now() / 1000;
+// if (localStorage.token) {
+//   setAuthorizationToken(localStorage.token);
+//   const decoded = jwt.decode(localStorage.token);
+//   const currentTime = Date.now() / 1000;
 
-  if (decoded.exp < currentTime) {
-    localStorage.removeItem('token');
-    store.dispatch(Logout());
-  } else {
-    store.dispatch(setCurrentUser(localStorage.token));
-  }
-}
+//   if (decoded.exp < currentTime) {
+//     localStorage.removeItem('token');
+//     console.log('Token removido!');
+//   } else {
+//     console.log('Agregando Token');
+//   }
+// }
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
