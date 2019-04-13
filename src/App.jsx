f7f4fbe6f807e5e62546563016202e955d32f866
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-04-13 19:23:38 -0400

    apply new layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,27 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 // Material UI Custom Theme
-import { CssBaseline } from '@material-ui/core';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import theme from './themes/Blue';
 
 // Layout
-// import Layout from './components/Layout';
-
-// Atoms
-import MainContainer from './components/Atoms/MainContainer';
-
-// Layout
+import Layout from './components/Layout';
 import HeaderBar from './components/Layout/HeaderBar';
 import FeatureBar from './components/Layout/FeatureBar';
 import FeatureBarTabs from './components/Layout/FeatureBarTabs';
-
-import PaperBase from './components/paperbase/Paperbase';
-
-const Home = () => <div>Home</div>;
+import MainContainer from './components/Layout/MainContainer';
 
 const App = () => {
-  console.log(theme);
   return (
     <MuiThemeProvider theme={theme}>
-      <>
-        <CssBaseline />
+      <Layout>
         <HeaderBar />
         <FeatureBar title="Authentication" />
         <FeatureBarTabs />
-        <PaperBase />
-      </>
+        <MainContainer>
+          <h1>Content</h1>
+        </MainContainer>
+      </Layout>
     </MuiThemeProvider>
   );
 };
