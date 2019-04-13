9eb6040adcdf7987f1fb2d4c4f0419c4e17123eb
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-04-13 19:21:38 -0400

    clean styles

--- src/components/Layout/FeatureBar/styles.js
+++ src/components/Layout/FeatureBar/styles.js
@@ -2,36 +2,10 @@ const colorWhite = 'rgba(255, 255, 255, 0.7)';
 
 const styles = () => ({
   toolbar: {
-    marginBottom: 8
+    padding: '12px 24px 4px'
   },
   button: {
     borderColor: colorWhite
-  },
-  rootCrumbs: {
-    padding: '16px 24px 4px',
-    display: 'flex',
-    alignItems: 'center'
-  },
-  backArrow: {
-    color: colorWhite,
-    padding: '8px 0',
-    width: 40,
-    margin: '0 8px 0 -8px',
-    '&:hover': {
-      color: '#fff'
-    }
-  },
-  crumb: {
-    textTransform: 'capitalize',
-    cursor: 'pointer',
-    fontWeight: 500,
-    color: colorWhite,
-    '&:hover': {
-      color: '#fff'
-    }
-  },
-  crumbArrow: {
-    width: 24
   }
 });
 export default styles;
