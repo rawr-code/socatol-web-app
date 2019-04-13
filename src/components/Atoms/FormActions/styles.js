f57354a528bb15e01aa39da719d6903900732066
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-04-13 15:16:43 -0400

    clean

--- src/components/Atoms/FormActions/styles.js
+++
@@ -1,9 +0,0 @@
-const styles = () => ({
-  root: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    padding: 12
-  }
-});
-
-export default styles;
--- src/components/MuiInputsRedux/index.js
+++
@@ -1,5 +0,0 @@
-import InlineDatePicker from './MuiInlineDatePicker';
-import TextField from './MuiTextField';
-import Select from './MuiSelect';
-
-export { InlineDatePicker, TextField, Select };
