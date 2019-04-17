1f7ba9cad8e3fd633e2d888bfc0b7a012639f5a8
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-04-17 08:50:18 -0400

    add Title

--- src/components/Layout/FeatureBar/index.jsx
+++ src/components/Layout/FeatureBar/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // Material UI
 import {
@@ -18,17 +19,17 @@ import { Help } from '@material-ui/icons';
 import styles from './styles';
 
 const FeatureBar = props => {
-  const { classes } = props;
+  const { classes, title } = props;
   return (
     <AppBar component="div" color="primary" position="static" elevation={0}>
       <Toolbar className={classes.toolbar}>
         <Grid container alignItems="center" spacing={0}>
           <Grid item xs>
             <Typography color="inherit" variant="h5">
-              Authentication
+              {title}
             </Typography>
           </Grid>
-          <Grid item>
+          {/* <Grid item>
             <Button
               className={classes.button}
               variant="outlined"
@@ -36,7 +37,7 @@ const FeatureBar = props => {
               size="small">
               Web setup
             </Button>
-          </Grid>
+          </Grid> */}
           <Grid item>
             <Tooltip title="Ayuda">
               <IconButton color="inherit">
@@ -48,6 +49,11 @@ const FeatureBar = props => {
       </Toolbar>
     </AppBar>
   );
+};
+
+FeatureBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired
 };
 
 // Apply styles
