455ef3a1308c31668fbec60c704e1b7a8ed8d3bd
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-04-18 17:50:56 -0400

    new header for Forms

---
+++ src/components/Molecules/FormHeader/styles.js
@@ -0,0 +1,16 @@
+const styles = theme => ({
+  root: {
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    padding: '24px 24px 16px',
+    backgroundColor: theme.palette.primary.main
+  },
+  title: {
+    color: theme.palette.common.white
+  },
+  subtitle: {
+    color: theme.palette.light
+  }
+});
+
+export default styles;
