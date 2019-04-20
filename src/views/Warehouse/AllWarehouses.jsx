79beae848c20196978a9db23e7a3bd5f9452f6d5
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-04-20 14:54:26 -0400

    add cards

--- src/views/Warehouse/AllWarehouses.jsx
+++ src/views/Warehouse/AllWarehouses.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import { Query } from 'react-apollo';
+import { Link } from 'react-router-dom';
+
+// Material UI
+import {
+  Grid,
+  Card,
+  CardHeader,
+  CardActions,
+  Avatar,
+  Button
+} from '@material-ui/core';
+
+// Icons
+import { Home } from '@material-ui/icons';
 
 // Layout
 import { MainContainer } from '../../components/Layout';
+
+// Molecules
+import ContentHeader from '../../components/Molecules/ContentHeader';
 
 // Queries
 import { GET_WAREHOUSES_QUERY } from '../../queries/Warehouse';
@@ -10,16 +27,55 @@ import { GET_WAREHOUSES_QUERY } from '../../queries/Warehouse';
 const AllWarehouses = props => {
   return (
     <MainContainer>
+      <ContentHeader
+        title="Todos los almacenes"
+        button={{
+          label: 'Añadir almacén',
+          to: '/inventario/almacen/nuevo'
+        }}
+      />
       <Query query={GET_WAREHOUSES_QUERY}>
         {({ loading, error, data }) => {
           if (loading) return 'Loading...';
           if (error) return `Error:${error.message}`;
 
-          console.log(data);
-          return <h2>Data loaded!</h2>;
+          const warehouses = data.getWarehouses;
+          console.log(warehouses);
+          if (warehouses.length < 1) return null;
+          return (
+            <Grid container spacing={24}>
+              {warehouses.map(warehouse => (
+                <Grid item xs={12} md={3} key={warehouse.id}>
+                  <Card>
+                    <CardHeader
+                      avatar={
+                        <Avatar style={{ backgroundColor: '#039be5' }}>
+                          <Home />
+                        </Avatar>
+                      }
+                      title={warehouse.name}
+                      subheader={warehouse.active ? 'Activo' : 'Inactivo'}
+                    />
+
+                    <CardActions>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        fullWidth
+                        component={Link}
+                        to={`/inventario/almacen/${warehouse.id}`}
+                        //
+                      >
+                        Administrar
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          );
         }}
       </Query>
-      <p>Warehouses</p>
     </MainContainer>
   );
 };
