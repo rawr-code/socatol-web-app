d68a2b71199d256284fdfd2cfd6dc6244d85e9db
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-04-20 22:44:47 -0400

    add routes for BankAccount

--- src/views/Treasury.jsx
+++ src/views/Treasury.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import FeatureBar from '../components/Layout/FeatureBar';
 import FeatureBarTabs from '../components/Layout/FeatureBarTabs';
 
 // Tabs Views
-import { AllWarehouses } from './Warehouse';
+
+// BankAccount
+import {
+  AllBankAccounts,
+  NewBankAccount,
+  DetailsBankAccount,
+  UpdateBankAccount
+} from './BankAccount';
 
 const Treasury = props => {
   const tabs = [
     {
       label: 'Cuentas bancarias',
-      component: AllWarehouses
+      to: '/cuentas-bancarias'
     }
   ];
   return (
@@ -21,10 +28,38 @@ const Treasury = props => {
   );
 };
 
-const TreasuryContainer = ({ match }) => {
+const TreasuryContainer = ({ match: { path }, location: { pathname } }) => {
+  const showHeader = pathname === `${path}/cuentas-bancarias`;
+
   return (
     <>
-      <Route path={match.path} component={Treasury} />
+      {showHeader && <Route path={path} component={Treasury} />}
+      <Switch>
+        <Redirect from={path} to={`${path}/cuentas-bancarias`} exact />
+
+        {/* Warehouse Routes */}
+        <Route
+          exact
+          path={`${path}/cuentas-bancarias`}
+          component={AllBankAccounts}
+        />
+        <Route
+          exact
+          path={`${path}/cuentas-bancarias/nuevo`}
+          component={NewBankAccount}
+        />
+        <Route
+          exact
+          path={`${path}/cuentas-bancarias/:id`}
+          component={DetailsBankAccount}
+        />
+        <Route
+          exact
+          path={`${path}/cuentas-bancarias/:id/editar`}
+          component={UpdateBankAccount}
+        />
+        <Route component={() => <div>404</div>} />
+      </Switch>
     </>
   );
 };
