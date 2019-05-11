1788003532b1de49938abad4aca338647af3d70b
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-05-11 15:03:23 -0400

    update index app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Switch, Route } from 'react-router-dom';
 
 // Material UI Custom Theme
-import { MuiThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
 import theme from './themes/Green';
 
-// Layout
-import Layout from './components/Layout';
-
-// Views
-import Incomes from './views/Incomes';
-import Expenses from './views/Expenses';
-import Inventory from './views/Inventory';
-import Treasury from './views/Treasury';
+import AppRouter from './routes';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <MuiThemeProvider theme={theme}>
-        <Layout>
-          <Switch>
-            <Route path="/ingresos" component={Incomes} />
-            <Route path="/gastos" component={Expenses} />
-            <Route path="/inventario" component={Inventory} />
-            <Route path="/tesoreria" component={Treasury} />
-          </Switch>
-        </Layout>
-      </MuiThemeProvider>
-    </BrowserRouter>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppRouter />
+    </MuiThemeProvider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
+import { createUploadLink } from 'apollo-upload-client';
 import { onError } from 'apollo-link-error';
 import { ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
 
 import App from './App';
 
@@ -17,22 +18,35 @@ const cache = new InMemoryCache();
 const Errors = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
     graphQLErrors.map(({ message, locations, path }) =>
-      console.log(
+      console.error(
         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
       )
     );
-  if (networkError) console.log(`[Network error]: ${networkError}`);
+  if (networkError) console.error(`[Network error]: ${networkError}`);
 });
 
 // Create an http link
-const httpLink = new HttpLink({
+const httpLink = createUploadLink({
   uri: 'http://localhost:5000/graphql',
   credentials: 'same-origin'
 });
 
+// Auth Context
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
 // Create an Apollo Client
 const client = new ApolloClient({
-  link: ApolloLink.from([Errors, httpLink]),
+  link: ApolloLink.from([Errors, authLink.concat(httpLink)]),
   cache
 });
 
