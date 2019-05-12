e01d28d982ac0136254d3e0e39297f13681cb78b
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-05-12 15:30:59 -0400

    add WS

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createUploadLink } from 'apollo-upload-client';
 import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
+import { ApolloLink, split } from 'apollo-link';
 import { setContext } from 'apollo-link-context';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 
 import App from './App';
 
@@ -31,6 +33,27 @@ const httpLink = createUploadLink({
   credentials: 'same-origin'
 });
 
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:5000/graphql`,
+  options: {
+    reconnect: true
+  }
+});
+
+const links = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
+
 // Auth Context
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
@@ -44,10 +67,26 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+// Fetch Options
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'cache-and-network',
+    errorPolicy: 'ignore'
+  },
+  query: {
+    fetchPolicy: 'network-only',
+    errorPolicy: 'all'
+  },
+  mutate: {
+    errorPolicy: 'all'
+  }
+};
+
 // Create an Apollo Client
 const client = new ApolloClient({
-  link: ApolloLink.from([Errors, authLink.concat(httpLink)]),
-  cache
+  link: ApolloLink.from([Errors, authLink.concat(links)]),
+  cache,
+  defaultOptions
 });
 
 ReactDOM.render(
